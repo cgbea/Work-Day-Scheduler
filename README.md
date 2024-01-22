02d43dd9ee8de1701b85d31420ad9f2603414395
cgbea <151942177+cgbea@users.noreply.github.com>
2024-01-22 22:37:39 +0000

    added descriptive comments to js and updated README

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Work-Day-Scheduler
+# Work Day Scheduler
+
+## Description
+
+This project's goal was to build on existing starter code to create a work-day planner that an employee can use to save their tasks and manage their time on a day-to-day basis.
+
+
+![Screenshot of deployed webpage on 2024-01-22](Scheduler_screen.png)
+
+## Installation
+n/a
+
+## Usage
+
+The website can be publically accessed at https://cgbea.github.io/Work-Day-Scheduler/
+
+## Credits
+The project brief and starter code was provided by edX as part of its Skills for Life front-end skills course. Javacript and JQuery documentation from [W3Schools](https://www.w3schools.com/), [Day.js](https://day.js.org/) and [mdn web docs](https://developer.mozilla.org/en-US/docs/Learn) were used in the making of this project. 
+
+## License
+MIT License
+
+Copyright (c) 2024 cgbea
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,67 +1,63 @@
-var currentDay = dayjs().format('dddd, MMMM Do');
+var currentDay = dayjs().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
-//Creating the rows for each time block.
+//Creating the rows for each time block, including a textarea for user input and a save button.
 var hours = [];
 
 for (let i = 8; i <= 18; i++) {
-    hours.push(i + ":00");
-    var row = $("<div>");
-    row.addClass("time-block row");
-    row.append("<div class='col hour'> </div> <div class='col-8'><textarea id='input-" + [i] +"' name='input' rows='5' cols='1'> </textarea> </div> <div class='col saveBtn'> <button type='button'><i id= 'save-" + [i] + "' class='fas fa-save fs-2 text'></i></button> </div>");
-    $(".container").append(row);
+  hours.push(i + ":00");
+  var row = $("<div>");
+  row.addClass("time-block row");
+  row.append(
+    "<div class='col hour'> </div> <div class='col-8'><textarea id='input-" +
+      [i] +
+      "' name='input' rows='5' cols='1'> </textarea> </div> <div class='col saveBtn'> <button type='button'><i id= 'save-" +
+      [i] +
+      "' class='fas fa-save fs-2 text'></i></button> </div>"
+  );
+  $(".container").append(row);
 
-    //Adding hours in the first column
-    row.find(".hour").text(hours[hours.length - 1]);
-    
-    isPast(i);
-    isPresent(i);
-    isFuture(i);
-  };
+  //Adding hours in the first column
+  row.find(".hour").text(hours[hours.length - 1]);
 
-  function isPast(i){
-    if (i < dayjs().hour()){
-      $("#input-" +i).addClass("past")
-    }
+  isPast(i);
+  isPresent(i);
+  isFuture(i);
+}
 
+// The next three functions style the timeblocks according to the time of day.
+function isPast(i) {
+  if (i < dayjs().hour()) {
+    $("#input-" + i).addClass("past");
   }
+}
 
-  function isPresent(i){
-    if (i === dayjs().hour()){
-      $("#input-" +i).addClass("present")
-    }
-
+function isPresent(i) {
+  if (i === dayjs().hour()) {
+    $("#input-" + i).addClass("present");
   }
-  
-  function isFuture(i){
-    if (i > dayjs().hour()){
-      $("#input-" +i).addClass("future")
-    }
+}
 
+function isFuture(i) {
+  if (i > dayjs().hour()) {
+    $("#input-" + i).addClass("future");
   }
+}
 
-  //Add click functionality to save button
-  // $(".saveBtn").append("<button type='button'><i id= 'save' class='fas fa-save fs-2 text'></i></button>")
 
-  // document.getElementById("save").addEventListener("click", function ()
-  //   { for (var i =8; i <=18; i++)
-  //     { var input = document.getElementById("input").value ;
-  //       localStorage.setItem("task", input) ;
-  // }  } );
-
-// Load saved data into textareas
+// Load saved data from localStorage into textareas
 for (let i = 8; i <= 18; i++) {
   var savedData = localStorage.getItem("task-" + i);
   if (savedData !== null) {
-      document.getElementById("input-" + i).value = savedData;
+    document.getElementById("input-" + i).value = savedData;
   }
 }
-  
 
-  for (let i = 8; i <= 18; i++) {
-    document.getElementById("save-" + i).addEventListener("click", function () {
-        var index = this.id.split("-")[1]; 
-        var input = document.getElementById("input-" + index).value;
-        localStorage.setItem("task-" + index, input);
-    });
+// Save input from each time block textarea to localStorage
+for (let i = 8; i <= 18; i++) {
+  document.getElementById("save-" + i).addEventListener("click", function () {
+    var index = this.id.split("-")[1];
+    var input = document.getElementById("input-" + index).value;
+    localStorage.setItem("task-" + index, input);
+  });
 }
